382ce7d906153bb36460594da81c0c9abdfa684e
Michael Hiiva <michaelhiiva@gmail.com>
2023-07-22 19:02:31 -0700

    Added SetTimeOut.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,12 @@ qouteId.textContent = adviceClient.getQouteId();
 
 const qoute = document.getElementById("advice_sectionadvice_text");
 qoute.textContent = adviceClient.getQoute();
+
+const advice_section__dice_btn = document.getElementById("advice_section__dice_btn");
+advice_section__dice_btn.addEventListener(function() {
+    setTimeout(() => {
+        qouteId.textContent = "Loading";
+        qoute.textContent = "Loading...";
+    }, 250);
+    adviceClient.sendRequest();
+});
