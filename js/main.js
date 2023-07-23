b5377bc4c216f4e1143c970f88a89d985ee70252
Michael Hiiva <michaelhiiva@gmail.com>
2023-07-23 11:56:13 -0700

    Fixed Some of the Issues.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-import AdviceClient from "./AdviceClient.js";
-
-const adviceClient = new AdviceClient("https://api.adviceslip.com/advice");
-window.onload = adviceClient.sendRequest();
+const request_url = "https://api.adviceslip.com/advice";
+window.onload = getAdvice(request_url);
 
 const qouteId = document.getElementById("advice_section__advice_id");
-qouteId.textContent = adviceClient.getQouteId();
-
 const qoute = document.getElementById("advice_section__advice_text");
-qoute.textContent = adviceClient.getQoute();
 
 const advice_section__dice_btn = document.getElementById("advice_section__dice_btn");
-advice_section__dice_btn.addEventListener("click", function() {
-    setTimeout(() => {
-        qouteId.textContent = "Loading";
-        qoute.textContent = "Loading...";
-    }, 250);
-    adviceClient.sendRequest();
+advice_section__dice_btn.addEventListener("click", async function() {
+    qoute.textContent = "Loading...";
+    const advice = await getAdvice(request_url);
+    qoute.textContent = advice;
 });
+
+function getAdvice(request_url) {
+    fetch(request_url)
+      .then((response) => response.json())
+      .then((data) => data.slip)
+      .then((data) => {
+        qouteId.textContent = data.id;
+        qoute.textContent = data.advice;
+      });
+}
